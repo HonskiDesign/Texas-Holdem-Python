86ea969eda31028f22a8cf5e78ef7f2c39e29213
HonskiDesign <garren@honski.design>
2021-06-13 20:20:03 -0700

    Move player class to it's own file.

---
+++ main/Player.py
@@ -0,0 +1,28 @@
+# Texas Hold Em' for the CLI
+
+## Player Class Object
+
+class player:
+    current_npcs = 0
+    def __init__(self, player_type=None):
+        self.player_type = player_type
+        self.name = self.get_name()
+        self.chips = 10000
+        self.hand = []
+        self.hand_rank = []
+        self.is_betting = False
+    
+    ## Methods to get Player names and name NPCs
+    def npc_name(self):
+        player.current_npcs += 1
+        name = "NPC_" + str(player.current_npcs)
+        return name
+
+    def get_name(self):
+        if self.player_type is None:
+            self.player_type = 'Human'
+            player_name = input("Now what's your name, my friend?\n> ")
+            return player_name
+        else:
+            return self.npc_name()
+ 
--- main/Poker_Main.py
+++ main/Poker_Main.py
@@ -6,38 +6,8 @@ from random import shuffle
 from Winning_Hands_Logic import hand_rank_compare
 from Winning_Hands_Logic import card_logic_start
 from Card_Deck import cards
+from Player import player
  #import NPC_Logic - not currently used
-
-#***************#
-
-## Player Class Object
-
-class player:
-    current_npcs = 0
-    def __init__(self, player_type=None):
-        self.player_type = player_type
-        self.name = self.get_name()
-        self.chips = 10000
-        self.hand = []
-        self.hand_rank = []
-        self.is_betting = False
-    
-    ## Methods to get Player names and name NPCs
-    def npc_name(self):
-        player.current_npcs += 1
-        name = "NPC_" + str(player.current_npcs)
-        return name
-
-    def get_name(self):
-        if self.player_type is None:
-            self.player_type = 'Human'
-            player_name = input("Now what's your name, my friend?\n> ")
-            return player_name
-        else:
-            return self.npc_name()
-    
-    
-#***************#
 
 ## Game Engine Class Object
 
