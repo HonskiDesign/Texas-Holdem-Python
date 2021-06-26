74a792e727c0d6e56707272d4be084fcf5c7d4c9
HonskiDesign <garren@honski.design>
2021-06-25 19:32:14 -0700

    add some TODOs

---
+++ main/Engine.py
@@ -0,0 +1,8 @@
+# Texas Hold Em' for the CLI
+# Written by GH on 6-25-2021
+
+
+# TODO move engine game logic to this file
+# TODO refactor game engine to be specific to handling the turn and game startup
+
+
