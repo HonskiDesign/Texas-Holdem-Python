f24f04acce7534975911b600eb2e4c9d446712d2
HonskiDesign <garren@honski.design>
2021-06-27 00:24:36 -0700

    add start structure, need to finish ascii art

--- main/Engine.py
+++ main/Engine.py
@@ -3,6 +3,7 @@
 
 # TODO move engine game logic to this file
 # TODO refactor game engine to be specific to handling the turn and game startup
+
 import itertools
 from random import shuffle
 from Winning_Hands_Logic import hand_rank_compare
@@ -23,16 +24,34 @@ class engine():
         self.current_dealer = None
         self.previous_dealer = None
 
+################# Game Startup ##################
+
+    # Intro - First thing player see
+    def game_start(self):
+        print("**********************************************************************")
+        print("**TTTTT*EEEEE*X***X***************************************************")
+        print("****T***E******X*X****************************************************")
+        print("****T***EEE*****X*****************************************************")
+        print("****T***E******X*X****************************************************")
+        print("****T***EEEEE*X***X***************************************************")
+        print("**********************************************************************")
+        print("**********************************************************************")
+        print("**********************************************************************")
+
     # new npcs based on how many the user wants.
     def npcs_to_play(self):
         while True:
             try:
-                num_npcs = int(input("How many NPCs would you like to play against \(1-7\)? "))
+                num_npcs = int(input("How many NPCs would you like to play against (1-7)"))
                 if 1 <= num_npcs <= 7:
                     break
             except:
                 print("Please enter a number between 1 and 7")
 
         for i in range(1, num_npcs+1):
-            self.player_roster.append(player("NPC_" + str(i)))
+            self.player_roster.append(player("NPC"))
+
+
+game = engine()
+game.game_start()
 
