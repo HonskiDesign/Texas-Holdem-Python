35699731f364d9ffc64fd679a41915e71a5f611f
HonskiDesign <garren@honski.design>
2021-06-27 16:55:33 -0700

    added simple steps to guide myself through the loop

--- main/Game_Start.py
+++ main/Game_Start.py
@@ -8,8 +8,20 @@ print("Welcome to Texas Hold'em")
 
 # Game Initialize
 game = engine()
+game.get_player()
 game.npcs_to_play()
+game.round_start()
+game.set_dealer()
 
 # Call Game Loop
 
 # Match End
+
+# TODO remove this section once game is working
+## DEBUG ##
+for player in game.player_roster:
+    print("---Player---")
+    print(player.name)
+    print(player.chips)
+    print(player.hand)
+    print(player.is_betting)
