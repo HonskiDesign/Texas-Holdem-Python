d1e01a4024f00a65f38bbc987f5bb20ea38f19e0
HonskiDesign <garren@honski.design>
2021-06-27 16:19:11 -0700

    add Game_Start.py to replace Poker_Main... eventually

---
+++ main/Game_Start.py
@@ -0,0 +1,15 @@
+# Texas Hold'em for the CLI
+# Written by GH on 6-27-2021
+
+from Engine import engine
+
+# Welcome Screen
+print("Welcome to Texas Hold'em")
+
+# Game Initialize
+game = engine()
+game.npcs_to_play()
+
+# Call Game Loop
+
+# Match End
